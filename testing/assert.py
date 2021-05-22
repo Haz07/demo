3f8aef7fc6de9771104be207a6fd3b428a834315
Haseeb <moh.dhaseeb720@gmail.com>
2021-05-22 10:53:43 +0530

    Airline 2

---
+++ testing/assert.py
@@ -0,0 +1,16 @@
+
+#     return x * x
+
+# assert square(10) == 100
+
+import math
+
+def is_prime(n)
+    if(n<2)
+        return False
+    else{
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0: 
+                return False
+        return True
+    }
